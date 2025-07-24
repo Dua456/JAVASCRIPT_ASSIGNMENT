1babac429d0c4245f36e79d4f40883f12412264d
/Dua456 <duaahusaainhussain@gmail.com>
2025-07-24 22:04:55 +0500

    ./

---
+++ Assignment_12/app.js
@@ -0,0 +1,8 @@
+//1. Write a custom function power ( a, b ), to calculate the value of a raised to b.
+
+function power(a, b){
+    return a ** b
+}
+
+console.log(power(4,3)); // 64
+console.log(power(5,2)); // 25
